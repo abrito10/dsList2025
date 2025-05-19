840320a12e3b6055f8c4ee9020807be66d581748
Adilson <abrito10@gmail.com>
2025-05-19 15:43:56 -0300

    Project created

---
+++ src/main/java/com/abrito/dsList2025/IntensivoJavaSpring2025Application.java
@@ -0,0 +1,13 @@
+package com.abrito.dsList2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoJavaSpring2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoJavaSpring2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abrito/dsList2025/IntensivoJavaSpring2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrito.dsList2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoJavaSpring2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
