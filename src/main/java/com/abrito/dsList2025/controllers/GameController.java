be701f4dfd13d0c836fa15d2ff671cdc6b1c26ca
Adilson <abrito10@gmail.com>
2025-05-20 10:30:40 -0300

    Get game by id

--- src/main/java/com/abrito/dsList2025/controllers/GameController.java
+++ src/main/java/com/abrito/dsList2025/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abrito.dsList2025.dto.GameDTO;
 import com.abrito.dsList2025.dto.GameMinDTO;
 import com.abrito.dsList2025.services.GameService;
 
@@ -20,6 +22,12 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
+		return result;		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
 		return result;		
 	}
 }
--- src/main/java/com/abrito/dsList2025/services/GameService.java
+++ src/main/java/com/abrito/dsList2025/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.abrito.dsList2025.dto.GameDTO;
 import com.abrito.dsList2025.dto.GameMinDTO;
 import com.abrito.dsList2025.entities.Game;
 import com.abrito.dsList2025.repositories.GameRepository;
@@ -15,9 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly =  true)
 	public List<GameMinDTO> findAll(){
 		List<Game>  result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
 	}
 
+	@Transactional(readOnly =  true)	
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
 }
