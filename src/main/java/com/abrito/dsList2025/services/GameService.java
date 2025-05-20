70b8d70a5f2fbc237869a55eb3a4e58b11a3648c
Adilson <abrito10@gmail.com>
2025-05-20 11:11:44 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/abrito/dsList2025/services/GameService.java
+++ src/main/java/com/abrito/dsList2025/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.abrito.dsList2025.dto.GameDTO;
 import com.abrito.dsList2025.dto.GameMinDTO;
 import com.abrito.dsList2025.entities.Game;
+import com.abrito.dsList2025.projections.GameMinProjection;
 import com.abrito.dsList2025.repositories.GameRepository;
 
 @Service
@@ -29,4 +30,11 @@ public class GameService {
 		GameDTO dto = new GameDTO(result);
 		return dto;
 	}
+
+	@Transactional(readOnly =  true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection>  result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();		
+	}
+
 }
